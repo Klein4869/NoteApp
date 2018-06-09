95aea73b5e2c4c56f11576eb92851e0c1e642dcc
Klein4869 <thd1998@live.cn>
2018-06-09 16:34:14 +0800

    1

--- main.js
+++ main.js
@@ -41,8 +41,8 @@ let template = [{
         role: 'paste'
     }, {
         label: 'Delete',
-        accelerator: 'Del',
-        role: 'del'
+        accelerator: 'Delete',
+        role: 'delete'
     }]
 }]
 
@@ -104,10 +104,13 @@ let mainWindow
 
 function createMainWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600})
+    mainWindow = new BrowserWindow({width: 800, height: 600, show: false})
 
     // and load the index.html of the app.
     mainWindow.loadFile('index.html')
+    mainWindow.once('ready-to-show', () =>{
+        mainWindow.show()
+    })
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
@@ -143,20 +146,13 @@ app.on('window-all-closed', function () {
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit()
     }
 })
 
 app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
         createMainWindow()
     }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
